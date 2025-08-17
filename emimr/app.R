d863a58481ca6994acad55e9af504d025d410d65
hiba-benaribi <hiba.benaribi@gmail.com>
2025-08-17 13:18:10 +0100

    Update app.R

--- emimr/app.R
+++ emimr/app.R
@@ -8,8 +8,9 @@ server <- function(input, output, session) {
   output$plot <- renderPlot({
     x <- rnorm(100)
     y <- -log10(runif(100))
-    plot(x, y, pch = 19, col = "blue", main = "Base R Volcano Plot")
+    plot(x, y, pch = 19, col = "blue", main = "Basic R Volcano Plot")
   })
 }
 
 shinyApp(ui, server)
+
